7631aa6c94293eebbc955a3c010cd90daa47c9ec
Ian Carmichael <ian027@revature.net>
2022-07-26 12:54:02 -0400

    submit RPSConsoleOOP 07-25-2022

---
+++ ConsoleApp/RPSConsoleOOP/Round.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RpsConsole2
+{
+    public class Round
+    {
+        public int playerWinsCounter
+        {
+            get
+            {
+                return this.playerWinsCounter;
+            }
+            set
+            {
+                playerWinsCounter++;
+            }
+        }
+
+        public int computerWinsCounter
+        {
+            get
+            {
+                return this.computerWinsCounter;
+            }
+            set
+            {
+                computerWinsCounter++;
+            }
+        }
+    }
+}
