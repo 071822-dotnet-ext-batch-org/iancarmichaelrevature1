3249adc7c0cb7f5f03cca1c63035571f46652807
Ian Carmichael <ian027@revature.net>
2022-07-28 20:48:56 -0400

    progress made on p1. constructors, instances, and properties

--- ProjectDirectory/FoundationsProject/ProjectOne/Employee.cs
+++ ProjectDirectory/FoundationsProject/ProjectOne/Employee.cs
@@ -4,19 +4,86 @@ using System.Linq;
 using System.Threading.Tasks;
 
 
-/*I imagine once we get to SQL, we'll all use a database to store the data here.
+/*
+.: Update: 07.28.2022; 
+After speaking with Mark after work on Thursday, I've decided to take the time to proceed with the project in C#.
+and refactor the code later after learning the technologies I'll be using in the future.
+
+.: v0.002: 07.27.2022;
+I imagine once we get to SQL, we'll all use a database to store the data here.
 Therefore, in the interest of not having to go back and refactor over and over, 
-I will just create a list of objects to serve as a template.*/
+I will just create a list of objects to serve as a template.
+*/
+
+
 
 namespace ProjectOne
 
 {
     public class Employee
     {
-        public int EmployeeId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
+
+// set the Employee's ID to be a private variable (for security purposes)
+        private int employeeId;
+// make EmployeeID a property so that it can be accessed outside of the class
+        public int EmployeeId
+        {
+            get { return employeeId; }
+            set { employeeId = value; }
+        }
+
+// set the Employee's first name to be a private variable
+        private string firstName;
+// make FirstName a property (same as above)
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+// set the Employee's last name to be a private variable 
+        private string lastName;
+// make LastName a property (same as above)
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+// set the Employee's full name to be a private variable
+        private string fullName;
+// make FullName a property (same as above)
+        public string FullName
+        {
+            get { return fullName; }
+            set { fullName = value; }
+        }
+
+// set the Employee's email to be a private variable
+        private string email;
+// make Email a property (same as above)
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+//default constructor (Unsure if this is necessary)
+/*Mark's notes: "BUT if you create a parameterized constructor, 
+                 you MUST create your own defualt constructor" */
+        public Employee()
+        {
+
+        }
+
+// parameterized constructor (like Mark showed me)
+public Employee(int employeeId, string firstName, string lastName, string fullName, string email)
+        {
+            this.EmployeeId = employeeId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.FullName = fullName;
+            this.Email = email;
+        }
     }
 }
--- ProjectDirectory/FoundationsProject/ProjectOne/Reimbursement.cs
+++ ProjectDirectory/FoundationsProject/ProjectOne/Reimbursement.cs
@@ -7,15 +7,27 @@ namespace ProjectOne
 {
     public class Reimbursement
     {
-        public int ReimbursementTypeId { get; set; }
-        public int ReimbursementAmount { get; set; }
+        public double ReimbursementTypeId { get; set; }
+        public double ReimbursementAmount { get; set; }
 
     }
 }
 
-/* Still trying to get the hang of these Getters and Setters,
-   as well as dealing with OOP design principles
+
+/* 
+.: v0.003: 07.28.2022;
+   Corrected a namespace error.
+   Feeling confident in the knowledge I have amassed thus far.
+*/
+
+/* 
+.: v0.002: 07.27.2022;
+   Still trying to get the hang of these Getters and Setters,
+   as well as dealing with OOP design principles.
    I need simple answers to stupid things like: 
-   "Are my variables I've declared in this class being accessed in my main method, or am I being redundant?"
+   "Are my variables I've declared in this class being accessed 
+   in my main method, or am I being redundant?"
    
-   On the bright side, I feel like I'm just a few stupid questions away from a serious 'Ah-hah!' Moment. */
+   On the bright side, I feel like I'm just a few stupid 
+   questions away from a serious 'Ah-hah!' Moment. 
+*/
