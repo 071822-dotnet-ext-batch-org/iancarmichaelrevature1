1fa808b67ee1afa8a01fd48a687b188f313bccfa
Ian Carmichael <ian027@revature.net>
2022-07-30 10:02:06 -0400

    display trailing zeroes among other small refactors

--- ProjectDirectory/FoundationsProject/ProjectOne/Program.cs
+++ ProjectDirectory/FoundationsProject/ProjectOne/Program.cs
@@ -6,19 +6,27 @@ using System.Threading.Tasks;
 
 /*This is just the planning phase of the program.*/
 
-//Update: v0.003: 07.28.2022;
-/*         Lessons learned today about namespaces and classes: 
+//Update: v0.004: 07.30.2022;
+/*         Lots of small refactors in Program.cs:
+           Also: I found a way to display trailing zeroes.
+                 (This is important since we're dealing
+                 with currency. Money ain't no game.)
+           TODO: Use Exception Handling to catch errors.
+                 (My dreams are haunted by these errors.)      
+           TODO: Decide on either/or a List and/or Dictionary
+                 to store data and mimic an SQL database.   */
+                 
+//v0.001:  Initial Birth of the program. 07.27.2022;
+/*v0.002:  Revised the blueprint of program to exclude redudant variables. 07.27.2022;
+           Also: added some simple dialogue box functionality to Program.cs
+           Also: Experimented with Properties and out parameters, I'll have 
+                 a few questions going into class tomorrow~ */
+/*v0.003:  Lessons learned today about namespaces and classes: 
            namespace was previously ExpenseReimbursementSystem. 
            My neural pathways are always under construction.
            Also: I instanciated the Employee class in Program.cs
            Also: I parameterized a constructor for Employee
                  and reorganized my getters and setters.    */
-
-//v0.001: Initial Birth of the program. 07.27.2022;
-/*v0.002: Revised the blueprint of program to exclude redudant variables. 07.27.2022;
-           Also: added some simple dialogue box functionality to Program.cs
-           Also: Experimented with Properties and out parameters, I'll have 
-                 a few questions going into class tomorrow~ */
 
 // PROJECT ONE: Expense reimbursement system
 namespace ProjectOne
@@ -29,7 +37,9 @@ namespace ProjectOne
         static void Main(string[] args)
         {
 
-Employee employee = new Employee();
+        Employee e = new Employee();
+        Reimbursement r = new Reimbursement();
+
         
 Restart1:
 // Console prompts the User to input employee ID
@@ -49,6 +59,7 @@ Restart1:
         if (String.Equals("Yes", employeeIDAnswer, StringComparison.OrdinalIgnoreCase))
         {
             Console.WriteLine("Thank you.");
+            e.EmployeeId = employeeId;
         }
         else
         {
@@ -76,6 +87,9 @@ Restart2:
         if (fullNameAnswer == "yes")
         {
             Console.WriteLine("Thank you, " + fullName + ".");
+            e.FirstName = firstName;
+            e.LastName = lastName;
+            e.FullName = fullName;
         }
         else
         {
@@ -94,6 +108,13 @@ the program will prompt the user to enter a valid email address*/
             Console.WriteLine("Please enter a valid email address: ");
             email = Console.ReadLine();
         }
+        if
+        (email.Contains("@revature.net"))
+        {
+            Console.WriteLine("Thank you, " + email + ".");
+            e.Email = email;
+        }
+
 
 // Console prompts the User to input the type of reimbursement they are requesting from the enum I made in ReimbursementTypes.cs
         Console.WriteLine("Please enter the type of reimbursement you are requesting: ");
@@ -103,10 +124,11 @@ the program will prompt the user to enter a valid email address*/
         Console.WriteLine("4. Other");
 
 // Convert the User's input to an integer
-        int ReimbursementTypeId = Convert.ToInt32(Console.ReadLine());
+        int reimbursementTypeId = Convert.ToInt32(Console.ReadLine());
+        r.ReimbursementTypeId = reimbursementTypeId;
 
 // if the User input is 1, the program will print out "Food," and ask if this is correct.
-        if (ReimbursementTypeId == 1)
+        if (reimbursementTypeId == 1)
         {
             Console.WriteLine("Food,");
             Console.WriteLine("Is this correct?");
@@ -121,7 +143,7 @@ the program will prompt the user to enter a valid email address*/
         }
 
 // if the User input is 2, the program will print out "Lodging," and ask if this is correct.
-        if (ReimbursementTypeId == 2)
+        if (reimbursementTypeId == 2)
         {
             Console.WriteLine("Lodging,");
             Console.WriteLine("Is this correct?");
@@ -131,9 +153,9 @@ the program will prompt the user to enter a valid email address*/
                 {
                     Console.WriteLine("Thank you for your submission.");
                 }
-    }
+        }
 // if the User input is 3, the program will print out "Travel," and ask if this is correct.
-        if (ReimbursementTypeId == 3)
+        if (reimbursementTypeId == 3)
         {
             Console.WriteLine("Travel,");
             Console.WriteLine("Is this correct?");
@@ -145,7 +167,7 @@ the program will prompt the user to enter a valid email address*/
                 }
         }
 // if the User input is 4, the program will print out "Other," and ask if this is correct.
-        if (ReimbursementTypeId == 4)
+        if (reimbursementTypeId == 4)
         {
             Console.WriteLine("Other,");
             Console.WriteLine("Is this correct?");
@@ -161,21 +183,29 @@ the program will prompt the user to enter a valid email address*/
                 }
         }
 
-
+Restart3:
 // Console prompts the User to input the amount of reimbursement they are requesting
         Console.WriteLine("Please enter the amount of reimbursement you are requesting: ");
-
-        // if the user input is not a numerical value, the program will prompt the user to re-enter the amount of reimbursement
         double amount = Convert.ToDouble(Console.ReadLine());
-        if (!int.TryParse(Console.ReadLine(), out int ReimbursementAmount))
+        // if the user input is not a numerical value, the program will prompt the user to re-enter the amount of reimbursement
+        if (amount.ToString().Any(char.IsLetter))
+        {
+            Console.WriteLine("Please enter a numerical value: ");
+            goto Restart3;
+        }
+        else
+        {
+        Console.WriteLine($"Thank you, {e.FullName}, you've input ${amount}.");
+            r.ReimbursementAmount = amount;
+        }
         {
             Console.WriteLine("Please enter a number. (It may help to omit the dollar ($) sign).");
         }
-        else if (ReimbursementAmount < 1)
+        if (r.ReimbursementAmount < 0.01)
         {
-            Console.WriteLine("Please enter a greater than 0.");
+            Console.WriteLine("Please enter a greater amount than $0.00.");
         }
-        else if (ReimbursementAmount  > 5000)
+        else if (r.ReimbursementAmount  > 5000)
         {
             Console.WriteLine("Please contact our Human Resources department for assistance with this issue.");
         }
@@ -184,12 +214,19 @@ the program will prompt the user to enter a valid email address*/
             Console.WriteLine("Thank you for your submission.");
         };
 
-// Convert the User's input to an integer
-        int reimbursementAmount = Convert.ToInt32(Console.ReadLine());
+/* 
+Recording r.Reimbursement(double) amount in a string so that any recorded trailing zeroes will be shown to the user.
+We are dealing with money after all, so it's important to account for every penny.
+*/
+        string amountString = r.ReimbursementAmount.ToString("0.00");
 
 // Confirm the User's input using string interpolation, I store the User's input values into new variables and reference them.
-        string confirmation = $"You have requested {reimbursementAmount} for {ReimbursementTypeId}.";
+        string confirmation = $"You have requested ${amountString} for reimbursement type {r.ReimbursementTypeId}.";
+
+        Console.WriteLine(confirmation);
+
+        Console.WriteLine($"Thank you, {e.FullName}.\nA confirmation email will be sent momentarily to {e.Email}\nPlease give our system time to process your request.");
         
         }
-    }
-}
+    }//EndOfClass
+}//EndOfNamespace
