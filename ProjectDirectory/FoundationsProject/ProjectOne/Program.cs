61249bed34e06685204ef90817c42a213b9656a7
Ian Carmichael <ian027@revature.net>
2022-07-31 13:18:08 -0400

    new method IdTryParse as solution to conversion exemption.

--- ProjectDirectory/FoundationsProject/ProjectOne/Program.cs
+++ ProjectDirectory/FoundationsProject/ProjectOne/Program.cs
@@ -6,9 +6,32 @@ using System.Threading.Tasks;
 
 /*This is just the planning phase of the program.*/
 
-//Update: v0.004: 07.30.2022;
-/*         Lots of small refactors in Program.cs:
-           Also: I found a way to display trailing zeroes.
+//Update: v0.005: 07.31.2022;
+/*         Made a new method to call TryParse and recursively loop
+           when the user inputs any letter during Employee ID input
+           process. Inadvertantly created a bug(?) where the user must
+           input a value again to proceed with the program, but I've 
+           accounted for it by using prompts to the user. Almost seems 
+           like an intentional layer of validation for user input.
+           Definitely keeping it for now.
+           TODO: Figure out how to refactor this code to bring it
+                 more in line with the principles of OOP.
+*/
+//v0.001:  Initial Birth of the program. 07.27.2022;
+/*v0.002:  Revised the blueprint of program to exclude redudant variables. 07.27.2022;
+           Also: added some simple dialogue box functionality to Program.cs
+           Also: Experimented with Properties and out parameters, I'll have 
+                 a few questions going into class tomorrow~     
+*/
+/*v0.003:  Lessons learned today about namespaces and classes: 
+           namespace was previously ExpenseReimbursementSystem. 
+           My neural pathways are always under construction.
+           Also: I instanciated the Employee class in Program.cs
+           Also: I parameterized a constructor for Employee
+                 and reorganized my getters and setters.        
+*/
+//v0.004:  Lots of small refactors in Program.cs. 07.30.2022; 
+/*         Also: I found a way to display trailing zeroes.
                  (This is important since we're dealing
                  with currency. Money ain't no game.)
            TODO: Use Exception Handling to catch errors.
@@ -24,20 +47,6 @@ using System.Threading.Tasks;
            The noobish else-if monstrosity at the bottom is
            now a proper switch statement.
 */
-                 
-//v0.001:  Initial Birth of the program. 07.27.2022;
-/*v0.002:  Revised the blueprint of program to exclude redudant variables. 07.27.2022;
-           Also: added some simple dialogue box functionality to Program.cs
-           Also: Experimented with Properties and out parameters, I'll have 
-                 a few questions going into class tomorrow~     
-*/
-/*v0.003:  Lessons learned today about namespaces and classes: 
-           namespace was previously ExpenseReimbursementSystem. 
-           My neural pathways are always under construction.
-           Also: I instanciated the Employee class in Program.cs
-           Also: I parameterized a constructor for Employee
-                 and reorganized my getters and setters.        
-*/
 
 // PROJECT ONE: Expense reimbursement system
 namespace ProjectOne
@@ -50,29 +59,34 @@ namespace ProjectOne
         Console.Clear();
         Employee e = new Employee();
         Reimbursement r = new Reimbursement();
+        int employeeId = 0;
 
-//display ReimbursementTypes ex: Travel, Food, etc
-        
+            static void IdTryParse(string input, out int output)
+            {
+                if (int.TryParse(input, out output))
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Thank you, \"{output}\" is a valid Employee ID.\nPlease enter it one more time, just to confirm your input.");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Sorry, but \"{input}\" is not a valid Employee ID.\n\nYou must enter either a NUMBER, or series of NUMBERS.");
+                    IdTryParse(Console.ReadLine(), out output);
+                }
+            }
+
 Restart1:
-// Console prompts the User to input employee ID
-        Console.WriteLine("Please enter your employee ID: ");
-        int employeeId = Convert.ToInt32(Console.ReadLine());
-
-// if the response is alphanumeric, the program will ask the user to re-enter the employee ID   
-        if (employeeId.ToString().Any(char.IsLetter))
-        {
-            Console.WriteLine("Please enter your numeric employee ID: ");
-            goto Restart1;
-        }
-
-// TODO: catch the error if the user enters a character instead of a number
-        
-
-        Console.WriteLine("You entered " + employeeId + ", is this your Employee ID number? Yes or No.");
+        Console.WriteLine("Welcome to Revature's Employee Expense Reimbursement System.\nPlease enter your employee ID: ");
+        IdTryParse(Console.ReadLine(), out employeeId);
+        employeeId = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("You've entered " + employeeId + ".\nThis is your Employee ID number, correct? Yes or No.");
         string employeeIDAnswer = Console.ReadLine();
         // if yes continue and if no, repeat the process
         if (String.Equals("Yes", employeeIDAnswer, StringComparison.OrdinalIgnoreCase))
         {
+            Console.Clear();
             Console.WriteLine("Thank you.");
             e.EmployeeId = employeeId;
         }
@@ -88,6 +102,7 @@ Restart2:
         string firstName = Console.ReadLine();
             
 // Console prompts the User to input employee last name
+        Console.Clear();
         Console.WriteLine("Please enter your last name: ");
         string lastName = Console.ReadLine();
 
@@ -95,12 +110,14 @@ Restart2:
         string fullName = firstName + " " + lastName;
 
 // ask the User if their fullName is correct
+        Console.Clear();
         Console.WriteLine("Is " + fullName + " your full name?");
         string fullNameAnswer = Console.ReadLine();
 
 // if yes continue and if no, repeat the process
         if (String.Equals("Yes", fullNameAnswer, StringComparison.OrdinalIgnoreCase))
         {
+            Console.Clear();
             Console.WriteLine("Thank you, " + fullName + ".");
             e.FirstName = firstName;
             e.LastName = lastName;
@@ -127,7 +144,8 @@ the program will prompt the user to enter a valid email address*/
         if
         (email.Contains("@revature.net"))
         {
-            Console.WriteLine("Thank you, " + e.FullName + ".");
+            Console.Clear();
+            Console.WriteLine("Thank you, " + e.FullName + ".\n");
             e.Email = email;
         }
 
@@ -148,6 +166,7 @@ Restart4:
 // if the User input is 1, the program will print out "Food," and ask if this is correct.
         if (reimbursementTypeId == 1)
         {
+            Console.Clear();
             reimbursementString = "1 (FOOD)";
             Console.WriteLine("Food,");
             Console.WriteLine("Is this correct?");
@@ -162,6 +181,7 @@ Restart4:
 // if the User input is 2, the program will print out "Lodging," and ask if this is correct.
         if (reimbursementTypeId == 2)
         {
+            Console.Clear();
             reimbursementString = "2 (LODGING)";
             Console.WriteLine("Lodging,");
             Console.WriteLine("Is this correct?");
@@ -175,6 +195,7 @@ Restart4:
 // if the User input is 3, the program will print out "Travel," and ask if this is correct.
         if (reimbursementTypeId == 3)
         {
+            Console.Clear();
             reimbursementString = "3 (TRAVEL)";
             Console.WriteLine("Travel,");
             Console.WriteLine("Is this correct?");
@@ -188,6 +209,7 @@ Restart4:
 // if the User input is 4, the program will print out "Other," and ask if this is correct.
         if (reimbursementTypeId == 4)
         {
+            Console.Clear();
             Console.WriteLine("Other,");
             Console.WriteLine("Is this correct?");
 
@@ -197,6 +219,7 @@ Restart4:
                     Console.WriteLine("Please specify what type of Reimbursement this is.");
 // store user response as a new type of reimbursement
                     otherReimbursement = Console.ReadLine();
+                    Console.Clear();
                     Console.WriteLine($"You've indicated this type of reimbursement is for {otherReimbursement}.");
                     Console.WriteLine("Thank you for your submission.");
                     reimbursementString = $"4 (OTHER): {otherReimbursement}";
@@ -249,7 +272,7 @@ We are dealing with money after all, so it's important to account for every penn
         if (String.Equals("Yes", confirmationAnswer, StringComparison.OrdinalIgnoreCase))
         {
             Console.Clear();
-            string summary = $"Before we continue on to finalizing your reimbursment request, here's the information you've submitted thus far:\nYour full name: {e.FullName}\nYour Employee ID number: {e.EmployeeId}\nYour email address: {e.Email}\nYour reimbursement type: {reimbursementString}\nYour reimbursement amount: ${amountString}\n\nIs this correct?\n";
+            string summary = $"Before we continue on to finalizing your reimbursment request, here's the information you've submitted thus far:\nYour full name: {e.FullName}\nYour Employee ID number: {e.EmployeeId}\nYour email address: {e.Email}\nYour reimbursement type: {reimbursementString}\nYour reimbursement amount: ${amountString}\n\nDoes all of this look correct?\n";
             Console.WriteLine(summary);
             confirmationAnswer2 = Console.ReadLine();
         }
